c425c757cb74e0bbd5f17d98e6de6d776bcba2d9
madhavakannan <49508988+madhavakannan@users.noreply.github.com>
2019-05-02 14:44:59 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i=0, a[5];
+	for(i=0;i<6;i++)
+	{
+		scanf("%d", &a[i]);
+		
+	}
+	for(i=0;i<6;i++)
+	{
+		printf("%d",a[i]);
+	}
+	return 0;
+}
